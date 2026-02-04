7a9082361ad3b4592558e8f0af45a37747a7332f
Alex-analiz <alexduck80@gmail.com>
2026-02-04 17:43:41 +0300

    Fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "alex-analiz.github.io/HMF",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
